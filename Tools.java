c095ce0a49903cf8cb80b18fb211c330ca1ca2b7
Your Name <you@example.com>
2018-11-27 17:53:37 +0530

    files are added

---
+++ Tools.java
@@ -0,0 +1,13 @@
+public class Tools {
+
+    public static void main(String[] args) {
+
+     for(int i=13;i<=15;i++){
+
+       System.out.println("Tools.."+i);
+    
+         }
+
+      }
+       
+   }
